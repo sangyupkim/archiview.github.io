c8337d02adac8aece0a3e0aa5b8b8cee1f244d07
sangyupkim <weak0416@gmail.com>
2025-11-14 09:48:02 +0900

    공연장업로드

---
+++ 관철동 공연장/data.js
@@ -0,0 +1,551 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "로비",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0637185410048957,
+          "pitch": 0.23109682910885887,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.0520507273466766,
+          "pitch": 0.22344820552613953,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "공연장입구",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.5476738216357173,
+        "pitch": 0.044810748057841465,
+        "fov": 1.4526150828746591
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6999690327073598,
+          "pitch": 0.2512548331310871,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.8710695647226032,
+          "pitch": 0.19546753996706023,
+          "rotation": 0,
+          "target": "4-x"
+        },
+        {
+          "yaw": -0.007491250754924295,
+          "pitch": 0.2011215818029033,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.088981416353816,
+          "pitch": 0.23816272055834276,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "입구층화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.9262630839000732,
+        "pitch": 0.027515701739680765,
+        "fov": 1.4526150828746591
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7205344265180935,
+          "pitch": 0.23100048157353115,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "공연장진입",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0274478400136857,
+          "pitch": 0.2995344444041983,
+          "rotation": 6.283185307179586,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.12718698369126713,
+          "pitch": 0.4802804002760084,
+          "rotation": 6.283185307179586,
+          "target": "9-"
+        },
+        {
+          "yaw": 0.11761253131492033,
+          "pitch": -0.03823286378901791,
+          "rotation": 9.42477796076938,
+          "target": "4-x"
+        },
+        {
+          "yaw": -2.866350199338104,
+          "pitch": 0.4880908937896269,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.3663081710161329,
+          "pitch": 0.19768043003065294,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-x",
+      "name": "공연장진입(커튼x)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12075776215655054,
+          "pitch": -0.048922375805048546,
+          "rotation": 3.141592653589793,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.0237766148626886,
+          "pitch": 0.2458801938440569,
+          "rotation": 6.283185307179586,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.21811563600699024,
+          "pitch": 0.3172073299608691,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": -1.3647371210745085,
+          "pitch": 0.19415791643451286,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "공연장오른쪽중단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6019266102563687,
+          "pitch": 0.238914112001531,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -3.011213962389693,
+          "pitch": -0.1208437422014903,
+          "rotation": 5.497787143782138,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.09208273034276715,
+          "pitch": 0.44588528690965745,
+          "rotation": 5.497787143782138,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "공연장오른쪽상단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.892483296966608,
+          "pitch": 0.5927584045677783,
+          "rotation": 0,
+          "target": "11-"
+        },
+        {
+          "yaw": -0.013516223641364178,
+          "pitch": 0.47980152208442384,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "공연장왼쪽중단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1385115221802504,
+          "pitch": 0.24984939902865655,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.033077497858089444,
+          "pitch": 0.44138486481708306,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 3.1282598768514633,
+          "pitch": -0.11316899630115884,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "공연장왼쪽상단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17929947675379942,
+          "pitch": 0.46948172803732113,
+          "rotation": 0.7853981633974483,
+          "target": "7-"
+        },
+        {
+          "yaw": -2.333113005867503,
+          "pitch": 0.09527450444108787,
+          "rotation": 5.497787143782138,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "무대",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1696746580447268,
+          "pitch": -0.023195260481873703,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.7687994520550454,
+          "pitch": -0.023368034706450658,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.8651525812479939,
+          "pitch": -0.031089219327288475,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "출구층엘리베이터",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.09039832373571777,
+        "pitch": 0.05414632056987756,
+        "fov": 1.4526150828746591
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.827212867810811,
+          "pitch": 0.10059287819799678,
+          "rotation": 0,
+          "target": "11-"
+        },
+        {
+          "yaw": -0.9051718008778433,
+          "pitch": 0.26459577091937625,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.3080519276455185,
+          "pitch": 0.4225395491827513,
+          "rotation": 5.497787143782138,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-",
+      "name": "출구층화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.415566592084593,
+          "pitch": 0.15435557347620588,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.6739331026676911,
+          "pitch": 0.11144752812661096,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

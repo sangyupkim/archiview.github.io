703ad516aa4e3c971f58a2aafc5f5f308305e610
sangyupkim <weak0416@gmail.com>
2025-11-14 23:30:15 +0900

    상가업로드

---
+++ shopping/data.js
@@ -0,0 +1,359 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "승강기홀",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28226425937557487,
+          "pitch": 0.16729456760922012,
+          "rotation": 0,
+          "target": "1-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-4",
+      "name": "4층계단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0366820881619159,
+          "pitch": 0.18973120388969988,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.8535361373856993,
+          "pitch": 0.17419856595570948,
+          "rotation": 0,
+          "target": "2-4"
+        },
+        {
+          "yaw": -0.15478722227959096,
+          "pitch": 0.5470261964483267,
+          "rotation": 0,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-4",
+      "name": "4층전망",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1152443539795573,
+          "pitch": 0.2057898840515957,
+          "rotation": 0,
+          "target": "1-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-3",
+      "name": "3층계단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21144275860784667,
+          "pitch": 0.5963075838754879,
+          "rotation": 0,
+          "target": "5-2"
+        },
+        {
+          "yaw": 3.1048305753447902,
+          "pitch": 0.006624111678975808,
+          "rotation": 0,
+          "target": "1-4"
+        },
+        {
+          "yaw": 0.8439120612840387,
+          "pitch": 0.2521485515181965,
+          "rotation": 5.497787143782138,
+          "target": "4-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-3",
+      "name": "3층전망",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5001465694749143,
+          "pitch": 0.2618743228818694,
+          "rotation": 0,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2",
+      "name": "2층계단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1304769358125384,
+          "pitch": 0.004912658349947918,
+          "rotation": 0,
+          "target": "3-3"
+        },
+        {
+          "yaw": -2.013848049657483,
+          "pitch": 0.3242057559672702,
+          "rotation": 1.5707963267948966,
+          "target": "7-1"
+        },
+        {
+          "yaw": 0.5296073165075974,
+          "pitch": 0.39062190364603033,
+          "rotation": 0,
+          "target": "6-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2",
+      "name": "2층전망",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6291994932185876,
+          "pitch": 0.31888592278654926,
+          "rotation": 5.497787143782138,
+          "target": "5-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-1",
+      "name": "1층계단",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4208318425781794,
+          "pitch": 0.3698375266727716,
+          "rotation": 5.497787143782138,
+          "target": "8-1"
+        },
+        {
+          "yaw": 2.9291645718297943,
+          "pitch": 0.03654155624156097,
+          "rotation": 5.497787143782138,
+          "target": "5-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-1",
+      "name": "1층전망",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.694615615644082,
+          "pitch": 0.3553270773677859,
+          "rotation": 0,
+          "target": "7-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
